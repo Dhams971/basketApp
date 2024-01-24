4d0ff5951f7e13c45c1935a78596d794a73884b4
Dharmendra Solanki <dhams971@gmail.com>
2024-01-25 02:07:25 +0530

    Project created

---
+++ basketApp/basketApp/basketAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  basketAppApp.swift
+//  basketApp
+//
+//  Created by DHARMENDRA SOLANKI on 25/01/24.
+//
+
+import SwiftUI
+
+@main
+struct basketAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
